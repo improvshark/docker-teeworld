5131f49f31a2e8ed9c9f78ca2db49c02661b4248
improvshark <improvshark@gmail.com>
2015-10-30 11:17:09 -0600

    yup

--- srv.py
+++ srv.py
@@ -14,10 +14,18 @@ def cmd (bashCommand):
 		print out
 	if err:
 		print err
+def copyConfig():
+        print 'updating config'
+        cmd("docker cp server.cfg %s:/opt/teeworld/server.cfg"%name)
 def launch ():
 	print 'begin launch'
 	cmd("docker run  --name %s -d --privileged=true --net host %s/%s" % (name,namespace,name) )
-
+def stop ():
+        print 'stoping %s'%name
+        cmd("docker stop %s"%name)
+def start ():
+        print 'starting %s'%name
+        cmd("docker start %s"%name)
 def build ():
 	print 'begin building image'
 	cmd("docker build -t %s/%s . " % (namespace,name) )
@@ -27,15 +35,23 @@ def main():
 	parser = argparse.ArgumentParser(description='Tool to manage a %s docker container' % name)
 	parser.add_argument('-l','--launch', help="Launch the game", action='store_true')
 	parser.add_argument('-b','--build', help="Build the container", action='store_true')
-	parser.add_argument('-s','--shell', help="Open a busybox shell in the container", action='store_true')
+	parser.add_argument('-s','--start', help="Launch the game", action='store_true')
+        parser.add_argument('-k','--kill', help="stop the game", action='store_true')
+	parser.add_argument('-c','--updateConfig', help="upate the game config within the container", action='store_true')
 	arg = parser.parse_args()
 	
 	if arg.launch:
 		launch()
-	if arg.build:
+	elif arg.build:
 		build()
-	if arg.shell:
+	elif arg.start:
+                start()
+        elif arg.kill:
+                stop()
+	elif arg.shell:
 		shell()
-	if not any(vars(arg).values()):
+	elif arg.updateConfig:
+		copyConfig()
+	elif not any(vars(arg).values()):
 		parser.print_help()
 main()
